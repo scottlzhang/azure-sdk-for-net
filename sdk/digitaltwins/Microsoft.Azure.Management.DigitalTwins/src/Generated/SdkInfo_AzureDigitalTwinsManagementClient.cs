bb9adf5b392251c78af2f1b086d604b4ac310d1e
David R. Williamson <drwill@microsoft.com>
2020-12-29 11:40:42 -0800

    Generate from updated source readme

--- sdk/digitaltwins/Microsoft.Azure.Management.DigitalTwins/src/Generated/SdkInfo_AzureDigitalTwinsManagementClient.cs
+++ sdk/digitaltwins/Microsoft.Azure.Management.DigitalTwins/src/Generated/SdkInfo_AzureDigitalTwinsManagementClient.cs
@@ -27,6 +27,16 @@ namespace Microsoft.Azure.Management.DigitalTwins
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/digitaltwins/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\code\\Azure\\asfn1\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "dab488ef4572b30dee885044331326f8d1c8d329";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
 
